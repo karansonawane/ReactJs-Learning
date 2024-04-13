057801ea9c571616aab719c122846ef7aff85166
karansonawane <sonawanekaran0007@gmail.com>
2024-04-13 11:07:29 +0530

    added new api key to fetch the movie details

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -51,7 +51,7 @@ import StarRating from "./StarRating";
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = "f84fc31d";
+const KEY = "fe57d955";
 
 export default function App() {
   const [query, setQuery] = useState("");
